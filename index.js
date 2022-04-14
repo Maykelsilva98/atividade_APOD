16be6c0ada1ac96bc9b83dc2197526605b7535d2
Maykelsilva98 <mayckelsantos5@gmail.com>
2022-04-14 18:00:51 -0300

    primeira versão finalizada

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 $("#submit").click(function (event){
     event.preventDefault();
-    let data = $("#data").val();
-    let urlS = "https://api.nasa.gov/planetary/apod?api_key=JOz79Y4Og1TuCJGje0ePJGCjORBFdDrR90dOwa40&date=" + data
+    const data = $("#data").val();
+    const urlS = "https://api.nasa.gov/planetary/apod?api_key=JOz79Y4Og1TuCJGje0ePJGCjORBFdDrR90dOwa40&date=" + data
     $.ajax({
         url: urlS,
         success: function (resposta) {
             if (resposta.media_type == "image"){
+                $("#video").attr("src", "")
                 $("#img").attr("src", resposta.url)
                 console.log(resposta.url)
             } else if (resposta.media_type == "video"){ 
                 console.log(resposta.url)
+                $("#img").attr("src", "")
                 $("#video").attr("src", resposta.url)
             }
         },
